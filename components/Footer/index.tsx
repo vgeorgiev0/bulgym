e648e1cb62887087ecf2138e229187bf96195e67
VGeorgiev <vgeorgiev@up2technology.com>
2023-11-23 21:14:57 +0200

    Add padding horizontal to the footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <>
       <footer className="mt-40 border-t border-stroke bg-white dark:border-strokedark dark:bg-blacksection">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-          <div className="flex flex-col flex-wrap items-center justify-center gap-5 border-t border-stroke py-7 dark:border-strokedark lg:flex-row lg:justify-between lg:gap-0">
+          <div className="flex flex-col flex-wrap items-center justify-center gap-5 border-t border-stroke py-7 dark:border-strokedark lg:flex-row lg:justify-between lg:gap-0 lg:px-16">
             <motion.div
               variants={{
                 hidden: {
