23ad14d393072571256be484cc7b582a773c33e0
VGeorgiev <vgeorgiev@up2technology.com>
2024-04-02 18:37:30 +0300

    Fix text-wrap issue inside of the PDFLink component

--- components/Docs/PdfLink.tsx
+++ components/Docs/PdfLink.tsx
@@ -14,10 +14,7 @@ const PdfLink: React.FC<PdfLinkProps> = ({ id, title }) => {
       className="mb-4 rounded-md bg-white p-4 shadow-md hover:shadow-lg"
     >
       <Link href={`/docs/${id}`}>
-        <p className="text-lime-900 hover:underline">
-          {title.substring(0, 34)}
-          {title.length > 34 && "..."}
-        </p>
+        <p className="text-wrap text-lime-900 hover:underline">{title}</p>
       </Link>
     </motion.div>
   );
